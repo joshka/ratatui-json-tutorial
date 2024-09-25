dd9a7ad1d264dbaff34c25b753d7236edff51648
Josh McKinney <joshka@users.noreply.github.com>
2024-09-25 04:41:08 -0700

    refactor: update to ratatui 0.28.1 and simplify some app logic

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,89 @@
 use std::{fs::File, path::PathBuf};
 
 use clap::Parser;
+use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
 
 use json_widget::JsonWidget;
-use tui::Tui;
+use ratatui::{
+    layout::{Constraint, Layout},
+    style::Stylize,
+    text::Line,
+    DefaultTerminal,
+};
 
-mod errors;
 mod json_widget;
-mod tui;
 
-fn main() -> color_eyre::Result<()> {
-    errors::install_hooks()?;
-    let tui = tui::init()?;
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let tui = ratatui::init();
     let cli = Cli::parse();
-    let mut app = JsonEditorApp::new(tui, cli.file_or_default());
-    app.run()?;
-    tui::restore()?;
-    Ok(())
+    let mut app = JsonEditorApp::new(tui, cli.file);
+    let result = app.run();
+    ratatui::restore();
+    result
 }
 
 #[derive(Debug, Parser)]
 struct Cli {
-    file: Option<PathBuf>,
-}
-
-impl Cli {
-    fn file_or_default(&self) -> PathBuf {
-        self.file
-            .as_ref()
-            .map(PathBuf::from)
-            .unwrap_or_else(|| PathBuf::from("demo.json"))
-    }
+    #[arg(default_value = "demo.json")]
+    file: PathBuf,
 }
 
 struct JsonEditorApp {
-    tui: Tui,
+    quit: bool,
+    tui: DefaultTerminal,
     file: PathBuf,
     json: JsonWidget,
 }
 
 impl JsonEditorApp {
-    fn new(tui: Tui, file: PathBuf) -> Self {
+    fn new(tui: DefaultTerminal, file: PathBuf) -> Self {
         Self {
+            quit: false,
             tui,
             file,
             json: JsonWidget::default(),
         }
     }
 
-    fn run(&mut self) -> color_eyre::Result<()> {
-        self.json.load(File::open(&self.file)?)?;
-        loop {
-            self.render()?;
-            if let Event::Key(event) = event::read()? {
-                if self.handle_key(event)? {
-                    break;
-                }
-            }
+    fn run(&mut self) -> Result<()> {
+        let reader = File::open(&self.file)?;
+        self.json.load(reader)?;
+        while !self.quit {
+            self.draw()?;
+            self.handle_events()?;
         }
         Ok(())
     }
 
-    fn render(&mut self) -> color_eyre::Result<()> {
+    fn draw(&mut self) -> Result<()> {
         self.tui.draw(|frame| {
-            frame.render_widget(&self.json, frame.size());
+            let [title, main] =
+                Layout::vertical([Constraint::Length(1), Constraint::Fill(1)]).areas(frame.area());
+            let line = Line::from("JSON editor tutorial example. [k prev] [j next] [q quit]")
+                .white()
+                .on_blue();
+            frame.render_widget(line, title);
+            frame.render_widget(&self.json, main);
         })?;
         Ok(())
     }
 
-    fn handle_key(&mut self, event: KeyEvent) -> color_eyre::Result<bool> {
-        match (event.modifiers, event.code) {
-            (_, KeyCode::Char('q')) => return Ok(true),
-            (_, KeyCode::Char('j')) => self.json.next_edit(),
-            (_, KeyCode::Char('k')) => self.json.prev_edit(),
+    fn handle_events(&mut self) -> Result<()> {
+        if let Event::Key(event) = event::read()? {
+            self.handle_key(event);
+        }
+        Ok(())
+    }
+
+    fn handle_key(&mut self, event: KeyEvent) {
+        use KeyCode::*;
+        match event.code {
+            Char('q') | Esc => self.quit = true,
+            Char('j') | Char('l') | Down | Right => self.json.next_edit(),
+            Char('k') | Char('h') | Up | Left => self.json.prev_edit(),
             _ => {}
         }
-        Ok(false)
     }
 }
